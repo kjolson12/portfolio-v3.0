3a835f253e05473976b6504c51499676a4aa96f7
olsonk <kyle.olson@ssninc.com>
2026-04-17 18:37:32 -0400

    added multiplier system to nodes

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -10,26 +10,35 @@ export default function Chip ({ title }: ChipProps) {
 
     const generateNodeStyle = (index: number, title: string | undefined) => {
         const unit = 17;
+        let multiplier = 1;
 
         if (title === 'online') {
             if (index < 4) {
-                return { top: `-${unit}px`, left: `${index * unit}px` };
+                if (index > 1) multiplier = 2;
+                return { top: `-${unit}px`, left: `${index * unit * multiplier}px` };
             } else if (index < 8) {
-                return { right: `-${unit}px`, top: `${(index - 4 )* unit}px` };
+                if (index > 6) multiplier = 2;
+                return { right: `-${unit}px`, top: `${(index - 4)* unit * multiplier}px` };
             } else if (index < 12) {
-                return { bottom: `-${unit}px`, right: `${(index - 8) * unit}px` };
+                if (index > 10) multiplier = 2;
+                return { bottom: `-${unit}px`, right: `${(index - 8) * unit * multiplier}px` };
             } else if (index < 16) {
-                return { left: `-${unit}px`, bottom: `${(index - 12) * unit}px` };
+                if (index > 13) multiplier = 2;
+                return { left: `-${unit}px`, bottom: `${(index - 12) * unit * multiplier}px` };
             }
         } else if (title === 'offline') {
             if (index < 4) {
-                return { top: `-${unit}px`, left: `${index * unit}px` };
+                if (index > 2) multiplier = 2;
+                return { top: `-${unit}px`, left: `${index * unit * multiplier}px` };
             } else if (index < 8) {
-                return { right: `-${unit}px`, top: `${(index - 4 )* unit}px` };
+                if (index > 5) multiplier = 2;
+                return { right: `-${unit}px`, top: `${(index - 4)* unit * multiplier}px` };
             } else if (index < 12) {
-                return { bottom: `-${unit}px`, right: `${(index - 8) * unit}px` };
+                if (index > 9) multiplier = 2;
+                return { bottom: `-${unit}px`, right: `${(index - 8) * unit * multiplier}px` };
             } else if (index < 16) {
-                return { left: `-${unit}px`, bottom: `${(index - 12) * unit}px` };
+                if (index > 13) multiplier = 2;
+                return { left: `-${unit}px`, bottom: `${(index - 12) * unit * multiplier}px` };
             }
         }
     }
