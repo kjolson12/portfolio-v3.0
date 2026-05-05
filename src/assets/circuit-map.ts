a548694c5aa0df75e1f3914a7c50348bcce8e62d
olsonk <kyle.olson@ssninc.com>
2026-05-05 12:16:15 -0400

    added offline circuits

--- src/assets/circuit-map.ts
+++ src/assets/circuit-map.ts
@@ -362,9 +362,439 @@ export const skillCircuitMap = {
             rotation: 180
         },
     ],
-    weightlifting: [],
-    espresso: [],
-    guitar: [],
-    videogames: [],
-    cooking: [],
+    weightlifting: [
+        {
+            version: 'straight',
+            coordinate: [28, 13],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [28, 12],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [28, 11],
+            rotation: 0
+        },
+        {
+            version: 'elbow2',
+            coordinate: [28, 10],
+            rotation: 270
+        },
+        {
+            version: 'diagonal',
+            coordinate: [29, 9],
+            rotation: 90
+        },
+        {
+            version: 'diagonal',
+            coordinate: [30, 8],
+            rotation: 90
+        },
+        {
+            version: 'elbow2',
+            coordinate: [31, 7],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [31, 6],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [31, 5],
+            rotation: 0
+        },
+        {
+            version: 'elbow',
+            coordinate: [31, 4],
+            rotation: 180
+        },
+        {
+            version: 'elbow2',
+            coordinate: [30, 3],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [29, 3],
+            rotation: 90
+        },
+    ],
+    espresso: [
+        {
+            version: 'straight',
+            coordinate: [31, 13],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [31, 12],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [31, 11],
+            rotation: 0
+        },
+        {
+            version: 'elbow2',
+            coordinate: [31, 10],
+            rotation: 270
+        },
+        {
+            version: 'diagonal',
+            coordinate: [32, 9],
+            rotation: 270
+        },
+        {
+            version: 'diagonal',
+            coordinate: [33, 8],
+            rotation: 270
+        },
+        {
+            version: 'elbow',
+            coordinate: [34, 7],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [35, 7],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [36, 7],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [37, 7],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [38, 7],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [39, 7],
+            rotation: 90
+        },
+    ],
+    guitar: [
+        {
+            version: 'diagonal',
+            coordinate: [21, 16],
+            rotation: 0
+        },
+        {
+            version: 'elbow2',
+            coordinate: [20, 15],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [19, 15],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [18, 15],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [17, 15],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [16, 15],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [15, 15],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [14, 15],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [13, 15],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [12, 15],
+            rotation: 90
+        },
+        {
+            version: 'elbow',
+            coordinate: [11, 15],
+            rotation: 90
+        },
+        {
+            version: 'diagonal',
+            coordinate: [10, 16],
+            rotation: 90
+        },
+        {
+            version: 'elbow2',
+            coordinate: [9, 17],
+            rotation: 270
+        },
+        {
+            version: 'straight',
+            coordinate: [9, 18],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [9, 19],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [9, 20],
+            rotation: 0
+        },
+    ],
+    videogames: [
+        {
+            version: 'straight',
+            coordinate: [25, 30],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [25, 31],
+            rotation: 0
+        },
+        {
+            version: 'elbow2',
+            coordinate: [25, 32],
+            rotation: 90
+        },
+        {
+            version: 'elbow',
+            coordinate: [24, 33],
+            rotation: 270
+        },
+        {
+            version: 'straight',
+            coordinate: [23, 33],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [22, 33],
+            rotation: 90
+        },
+        {
+            version: 'elbow',
+            coordinate: [21, 33],
+            rotation: 90
+        },
+        {
+            version: 'diagonal',
+            coordinate: [20, 34],
+            rotation: 90
+        },
+        {
+            version: 'elbow2',
+            coordinate: [19, 35],
+            rotation: 270
+        },
+        {
+            version: 'straight',
+            coordinate: [19, 36],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [19, 37],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [19, 38],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [34, 30],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [34, 31],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [34, 32],
+            rotation: 0
+        },
+        {
+            version: 'elbow',
+            coordinate: [34, 33],
+            rotation: 0
+        },
+        {
+            version: 'diagonal',
+            coordinate: [35, 34],
+            rotation: 0
+        },
+        {
+            version: 'diagonal',
+            coordinate: [36, 35],
+            rotation: 0
+        },
+        {
+            version: 'elbow2',
+            coordinate: [37, 36],
+            rotation: 180
+        },
+        {
+            version: 'straight',
+            coordinate: [38, 36],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [39, 36],
+            rotation: 90
+        },
+        {
+            version: 'elbow2',
+            coordinate: [40, 36],
+            rotation: 0
+        },
+        {
+            version: 'elbow',
+            coordinate: [41, 37],
+            rotation: 180
+        },
+        {
+            version: 'straight',
+            coordinate: [41, 38],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [41, 39],
+            rotation: 0
+        },
+        {
+            version: 'elbow2',
+            coordinate: [41, 40],
+            rotation: 90
+        },
+        {
+            version: 'elbow',
+            coordinate: [40, 41],
+            rotation: 270
+        },
+        {
+            version: 'straight',
+            coordinate: [39, 41],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [38, 41],
+            rotation: 90
+        },
+    ],
+    cooking: [
+        {
+            version: 'straight',
+            coordinate: [38, 20],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [39, 20],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [40, 20],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [41, 20],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [42, 20],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [43, 20],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [44, 20],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [45, 20],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [46, 20],
+            rotation: 90
+        },
+        {
+            version: 'straight',
+            coordinate: [47, 20],
+            rotation: 90
+        },
+        {
+            version: 'elbow2',
+            coordinate: [48, 20],
+            rotation: 0
+        },
+        {
+            version: 'diagonal',
+            coordinate: [49, 21],
+            rotation: 0
+        },
+        {
+            version: 'diagonal',
+            coordinate: [50, 22],
+            rotation: 0
+        },
+        {
+            version: 'elbow',
+            coordinate: [51, 23],
+            rotation: 180
+        },
+        {
+            version: 'straight',
+            coordinate: [51, 24],
+            rotation: 0
+        },
+        {
+            version: 'straight',
+            coordinate: [51, 25],
+            rotation: 0
+        },
+    ],
 };
