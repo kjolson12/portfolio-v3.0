f1e9ce47767fa0d5e1d2a0facb6cba38f4b5e431
olsonk <kyle.olson@ssninc.com>
2026-05-12 11:22:26 -0400

    started footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,12 +3,42 @@ import './Footer.css';
 export default function Footer() {
     return (
         <footer>
-            <p>© 2026 My Company. All rights reserved.</p>
-            <nav>
-                <a href="/privacy">Privacy Policy</a>
-                <a href="/terms">Terms of Service</a>
-                <a href="/contact">Contact Us</a>
-            </nav>
+                <div id="bio-image-and-title-footer">
+                    <div id="bio-image-footer" className='hover-animation'></div>
+                    <div id="bio-title-footer">
+                        <h3>Kyle Olson</h3>
+                        <h4>Website Developer</h4>
+                        <div id="socials">
+                            <div className="social-container">
+                                <a href="" target="_blank" rel="noopener noreferrer">
+                                    <i className="bi bi-linkedin"></i>
+                                </a>
+                            </div>
+                            <div className="social-container">
+                                <a href="" target="_blank" rel="noopener noreferrer">
+                                    <i className="bi bi-github"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <div id="contact">
+                <h3>Get in touch</h3>
+                <div id="contact-info-container">
+                    <div className='contact-info'>
+                        <p className='contact-info-category'>email</p>
+                        <p className='contact-info-value'>
+                            <a href="mailto:kjolson2016@gmail.com">kjolson2016@gmail.com</a>
+                        </p>
+                    </div>
+                    <div className='contact-info'>
+                        <p className='contact-info-category'>phone</p>
+                        <p className='contact-info-value'>
+                            <a href="tel:714-873-5571">714-873-5571</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
