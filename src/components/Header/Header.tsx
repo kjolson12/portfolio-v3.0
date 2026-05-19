1a7a6737256dec559d4beac2f87604407c3bdf56
olsonk <kyle.olson@ssninc.com>
2026-05-19 12:59:32 -0400

    added mobile nav component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,47 @@
+import { useState, useEffect } from 'react';
+
 import './Header.css';
+
+import MobileNav from '../MobileNav/MobileNav';
 
 const headerLogoStart = "<";
 const headerLogoEnd = "/>";
 
 export default function Header ({ activeSection }: { activeSection: string }) {
-    return (
-    <header>
-        <div id="name-home-link-container">
-            <a id="name-home-link" href='#bio' className="typewriter">
-                <span className='headerLogo'>{headerLogoStart}</span> Kyle Olson <span className='headerLogo'>{headerLogoEnd}</span>
-            </a>
-        </div>
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const desktopNav = (
         <nav id="main-nav">
             <a className={`main-nav-link ${activeSection === 'experience' ? 'active' : ''}`} href="#experience">Experience</a>
             <a className={`main-nav-link ${activeSection === 'professional' ? 'active' : ''}`} href="#professional">Professional Work</a>
             <a className={`main-nav-link ${activeSection === 'skills' ? 'active' : ''}`} href="#skills">Skills</a>
             <a className={`main-nav-link ${activeSection === 'personal' ? 'active' : ''}`} href="#personal">Personal Projects</a>
         </nav>
-        <div>
-            <button className='button button-secondary'>
-                Download Resume
-                {/* <i id="resume-download-icon" className="bi bi-download"></i> */}
-            </button>
-        </div>
-    </header>
+    );
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        
+        window.addEventListener('resize', handleResize);
+        
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <header>
+            <div id="name-home-link-container">
+                <a id="name-home-link" href='#bio' className="typewriter">
+                    <span className='headerLogo'>{headerLogoStart}</span> Kyle Olson <span className='headerLogo'>{headerLogoEnd}</span>
+                </a>
+            </div>
+
+            { windowWidth > 1024 ? desktopNav : <MobileNav activeSection={activeSection} /> }
+
+            <div>
+                <button className='button button-secondary'>Download Resume</button>
+            </div>
+        </header>
     );
 };
