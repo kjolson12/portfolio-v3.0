bc292ca58a0d9c6b16354a7e5dd19f9e45f15742
olsonk <kyle.olson@ssninc.com>
2026-05-19 15:26:42 -0400

    mobile nav complete

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import './MobileNav.css';
 
 const menuOptions = [
+    { name: 'Bio', href: '#bio' },
     { name: 'Experience', href: '#experience' },
     { name: 'Professional', href: '#professional' },
     { name: 'Skills', href: '#skills' },
@@ -13,25 +14,28 @@ export default function MobileNav ({ activeSection }: { activeSection: string })
     const [navOpen, setNavOpen] = useState(false);
     const [activeNavLink, setActiveNavLink] = useState(activeSection);
 
+    const cleanedMenuOptions = menuOptions.filter(option => option.href.substring(1) !== activeNavLink);
+
     useEffect(() => {
         setActiveNavLink(activeSection);
     }, [activeSection]);
 
     return (
         <nav id="main-nav-mobile" onClick={() => setNavOpen(!navOpen)}>
-            <button className={`button mobile-nav-button`}>
+            <a className={`button mobile-nav-button`}>
                 {activeNavLink}
                 <i className="bi bi-list"></i>
-            </button>
+            </a>
             <div id="mobile-nav-menu" className={navOpen ? 'open' : ''}>
-                { menuOptions.map(option => (
-                    <button 
+                { cleanedMenuOptions.map(option => (
+                    <a 
                         key={option.name} 
                         className={`button mobile-nav-button ${activeNavLink === option.href.substring(1) ? 'active' : ''}`} 
+                        href={option.href}
                         onClick={() => setNavOpen(false)}
                     >
                         {option.name}
-                    </button>
+                    </a>
                 )) }
             </div>
         </nav>
