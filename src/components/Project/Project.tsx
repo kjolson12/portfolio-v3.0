4678567cedddea75de8cf02986b1657313445744
olsonk <kyle.olson@ssninc.com>
2026-06-04 15:08:28 -0400

    setup for project responsiveness

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -15,18 +15,20 @@ export default function Project({ title, description, image, projectUrl, project
     return (
         <div className='project-container'>
             <img id={`${id}-img`} src={image} alt={`screenshot of ${title} website`} onClick={() => window.open(projectUrl, '_blank')} />
-            <div className="project-tags">
-                {projectTags?.map((tag, index) => (
-                    <span key={index} className={`project-tag ${tag.toLowerCase()}-tag`}>{tag}</span>
-                ))}
-            </div>
-            <div className="project-text">
-                <h5>{title}</h5>
-                <p>{description}</p>
-            </div>
-            <div className="project-button-container">
-                <button className="button" onClick={() => window.open(projectUrl, '_blank')}>View Website</button>
-                {projectGitHubUrl && <button className="button button-tertiary" onClick={() => window.open(projectGitHubUrl, '_blank')}>View GitHub</button>}
+            <div className="project-content">
+                <div className="project-tags">
+                    {projectTags?.map((tag, index) => (
+                        <span key={index} className={`project-tag ${tag.toLowerCase()}-tag`}>{tag}</span>
+                    ))}
+                </div>
+                <div className="project-text">
+                    <h5>{title}</h5>
+                    <p>{description}</p>
+                </div>
+                <div className="project-button-container">
+                    <button className="button" onClick={() => window.open(projectUrl, '_blank')}>View Website</button>
+                    {projectGitHubUrl && <button className="button button-tertiary" onClick={() => window.open(projectGitHubUrl, '_blank')}>View GitHub</button>}
+                </div>
             </div>
         </div>
     );
